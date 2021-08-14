9c6f1448b5c3d24cf6ba0bc9d38684daf792d8ae
JonasStempickij <jonasstempickij@gmail.com>
2021-08-15 01:48:46 +0300

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+//GLOBALS - NO WINDOW !!!
+
+//_dirname      -   path to current directory
+// _filename    -   file name
+// require      -   function to use modules (CommonJS)
+// module       -   info abour current module (file)
+// process      - info about env where the program is being executed
+
+console.log(__dirname)
+// console.log(module)
+setInterval(()=>{
+    console.log("hello world")
+}, 1000)
